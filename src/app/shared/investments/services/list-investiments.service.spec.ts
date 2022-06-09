97f3556716bb356261fa60d1f7c88d2b35e6354e
João Jacinto <joaonetotfm@gmail.com>
2022-06-09 20:00:32 -0300

    feature: configuração inicial do teste do serviço

--- src/app/shared/investments/services/list-investiments.service.spec.ts
+++ src/app/shared/investments/services/list-investiments.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ListInvestimentsService } from './list-investiments.service';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 describe('ListInvestimentsService', () => {
   let service: ListInvestimentsService;
-
+  let httpTestingController: HttpTestingController;
+  let httpClient: HttpClient;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    httpClient = TestBed.inject(HttpClient)
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(ListInvestimentsService);
   });
 
