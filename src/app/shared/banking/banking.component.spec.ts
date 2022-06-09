99d01ececfa643cb9c7614dc898c628e44c50f8f
João Jacinto <joaonetotfm@gmail.com>
2022-06-09 14:34:18 -0300

    feature: adiciona testes de tela

--- src/app/shared/banking/banking.component.spec.ts
+++ src/app/shared/banking/banking.component.spec.ts
@@ -31,38 +31,51 @@ describe('BankingComponent', () => {
   });
 
   it('should poupanca = 140 when calls setSacar("10")', () => {
-    component.setSacar('10')
-    expect(component.getPoupanca).toEqual(140)
-    expect(component.getCarteira).toEqual(590)
+    component.setSacar('10');
+    expect(component.getPoupanca).toEqual(140);
+    expect(component.getCarteira).toEqual(590);
   });
 
   it('should poupanca = 160 when calls setDepositar("10")', () => {
-    component.setDepositar('10')
-    expect(component.getPoupanca).toEqual(160)
-    expect(component.getCarteira).toEqual(570)
+    component.setDepositar('10');
+    expect(component.getPoupanca).toEqual(160);
+    expect(component.getCarteira).toEqual(570);
   });
 
   it('should donts acceppt string values on parameter setDepositar', () => {
-    expect(component.setDepositar('string')).not.toBeTruthy()
-    expect(component.getPoupanca).toEqual(150)
-    expect(component.getCarteira).toEqual(580)
+    expect(component.setDepositar('string')).not.toBeTruthy();
+    expect(component.getPoupanca).toEqual(150);
+    expect(component.getCarteira).toEqual(580);
   });
 
   it('should donts acceppt less than values on parameter setDepositar', () => {
-    expect(component.setDepositar('581')).not.toBeTruthy()
-    expect(component.getPoupanca).toEqual(150)
-    expect(component.getCarteira).toEqual(580)
+    expect(component.setDepositar('581')).not.toBeTruthy();
+    expect(component.getPoupanca).toEqual(150);
+    expect(component.getCarteira).toEqual(580);
   });
 
   it('should donts acceppt string values on parameter setSacar', () => {
-    expect(component.setSacar('string')).not.toBeTruthy()
-    expect(component.getPoupanca).toEqual(150)
-    expect(component.getCarteira).toEqual(580)
+    expect(component.setSacar('string')).not.toBeTruthy();
+    expect(component.getPoupanca).toEqual(150);
+    expect(component.getCarteira).toEqual(580);
   });
 
   it('should donts acceppt less than values on parameter setSacar', () => {
-    expect(component.setSacar('151')).not.toBeTruthy()
-    expect(component.getPoupanca).toEqual(150)
-    expect(component.getCarteira).toEqual(580)
+    expect(component.setSacar('151')).not.toBeTruthy();
+    expect(component.getPoupanca).toEqual(150);
+    expect(component.getCarteira).toEqual(580);
+  });
+
+  it('should show on display poupanca value', () => {
+    let el = fixture.debugElement.nativeElement;
+    el.querySelector('#input-depositar').value = '10';
+    el.querySelector('#depositar').click();
+    fixture.detectChanges(); //detectar as mudanças no DOM
+    const carteira = el.querySelector('#get-carteira')
+    const poupanca = el.querySelector('#get-poupanca')
+    expect(component.getPoupanca).toEqual(160);
+    expect(component.getCarteira).toEqual(570);
+    expect(carteira.innerHTML).toEqual('570');
+    expect(poupanca.innerHTML).toEqual('160');
   });
 });
