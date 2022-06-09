30867a78f7a7a26f8bf68c418f26917f57ffd664
João Jacinto <joaonetotfm@gmail.com>
2022-06-09 11:30:00 -0300

    feature: finaliza testes unitários das funções de sacar e depositar

--- src/app/shared/banking/banking.component.spec.ts
+++ src/app/shared/banking/banking.component.spec.ts
@@ -26,4 +26,21 @@ describe('BankingComponent', () => {
   it('should poupanca = 150 when calls getPopupanca()', () => {
     expect(component.getPoupanca).toEqual(150);
   });
+  it('should carteira = 580 when calls getCarteira()', () => {
+    expect(component.getCarteira).toEqual(580);
+  });
+
+  it('should poupanca = 140 when calls setSacar("10")', () => {
+    component.setSacar('10')
+    fixture.detectChanges()
+    expect(component.getPoupanca).toEqual(140)
+    expect(component.getCarteira).toEqual(590)
+  });
+
+  it('should poupanca = 160 when calls setDepositar("10")', () => {
+    component.setDepositar('10')
+    fixture.detectChanges()
+    expect(component.getPoupanca).toEqual(160)
+    expect(component.getCarteira).toEqual(570)
+  });
 });
