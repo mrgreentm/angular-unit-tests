2cb070bf5776516ba10c687a3353f38b06ad0bc6
João Jacinto <joaonetotfm@gmail.com>
2022-06-09 15:17:32 -0300

    feature: implementa primeiros testes (ainda sem serviços que constulta apis externas)

--- src/app/shared/investments/components/list/list.component.spec.ts
+++ src/app/shared/investments/components/list/list.component.spec.ts
@@ -8,9 +8,8 @@ describe('ListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ListComponent ]
-    })
-    .compileComponents();
+      declarations: [ListComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +20,21 @@ describe('ListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should investiments has three values', () => {
+    const investiments = component.investiments;
+    expect(investiments[0].name).toEqual('Fundos Imobiliários');
+    expect(investiments[2].name).toEqual('Tesouro Direto');
+    expect(investiments.length).toEqual(3);
+  });
+
+  it('should list investiments with three values', () => {
+    const investimentsList =
+      fixture.debugElement.nativeElement.querySelectorAll('.list-element');
+    expect(investimentsList.length).toEqual(3);
+    expect(investimentsList[0].innerHTML.trim()).toEqual(
+      'Fundos Imobiliários - R$ 18000'
+    );
   });
 });
